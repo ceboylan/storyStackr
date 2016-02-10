5ab91d72484627cfdfa0ee78814c35b54f597144
ceboylan <ceboylan0@gmail.com>
2016-02-10 10:58:18 -0600

    remove SQL dependencies for now

--- topify/views.py
+++ topify/views.py
@@ -1,4 +1,3 @@
-from a_Model import ModelIt
 from flask import request
 from flask import render_template
 from topify import app
@@ -7,24 +6,24 @@ from sqlalchemy_utils import database_exists, create_database
 import pandas as pd
 import psycopg2
 
-user = 'ceboylan' #add your username here (same as previous postgreSQL)                      
-host = 'localhost'
-dbname = 'topify_db'
-db = create_engine('postgres://%s%s/%s'%(user,host,dbname))
-con = None
-con = psycopg2.connect(database = dbname, user = user)
-
-if not database_exists(db.url):
-    create_database(db.url)
+# user = 'ceboylan' #add your username here (same as previous postgreSQL)
+# host = 'localhost'
+# dbname = 'topify_db'
+# db = create_engine('postgres://%s@%s/%s'%(user,host,dbname))
+# con = None
+# con = psycopg2.connect(database = dbname, user = user)
+#
+# if not database_exists(db.url):
+#     create_database(db.url)
 master = pd.DataFrame.from_csv('master_topic_info_sums_imgs.csv')
-master.to_sql('master_data_table', db, if_exists='replace')
+# master.to_sql('master_data_table', db, if_exists='replace')
 
-@app.route('/')
+# @app.route('/')
 @app.route('/index')
 def index():
    return "Hello, World!"
    
-@app.route('/input')
+@app.route('/')
 def cesareans_input():
     return render_template("input.html")
 
@@ -37,10 +36,12 @@ def cesareans_output():
   fromUser = tags+' '+summary
   # genre_label = str(patient)
     #just select the topic info from the topify database for the genre that the user inputs
-  query = "SELECT topic_proportion, topic_num, rep_url, rep_summary, term_str, img FROM master_data_table WHERE genre='%s'" % patient
-  print query
-  query_results=pd.read_sql_query(query,con)
-  print query_results
+  # query = "SELECT topic_proportion, topic_num, rep_url, rep_summary, term_str, img FROM master_data_table WHERE genre='%s'" % patient
+  # print query
+  # query_results=pd.read_sql_query(query,con)
+  # print query_results
+  query_results = master.loc[(master['genre']==patient)]
+  
   topics = []
   keyinput = ''
   for i in range(0,query_results.shape[0]):
@@ -49,7 +50,7 @@ def cesareans_output():
       keyinput += ', '
       keywords = keyinput.split(", ")
   keywords.remove('')
-  keywords.remove('None')    
+  # keywords.remove('None')
   matching_keywords = []
   missing_keywords = []
   for keyword in keywords:
